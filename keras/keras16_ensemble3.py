15bced9cc368d10e0d4534149bda3f518e17172d
4dv3ntur3 <allyep13.07@gmail.com>
2020-11-11 20:29:58 +0900

    2020-11-11 20:29

--- keras/keras16_ensemble3.py
+++ keras/keras16_ensemble3.py
@@ -15,9 +15,17 @@ x1 = x1.T
 x2 = x2.T
 y1 = y1.T
 
-
 #data 분리 (인자 3개까지 가능하므로 둘 중 하나에 y3 추가해도 됨)
 from sklearn.model_selection import train_test_split
+
+
+#난수 고정 
+import tensorflow as tf
+import random as python_random
+np.random.seed(123)
+python_random.seed(123)
+tf.random.set_seed(1234)
+
 x1_train, x1_test, x2_train, x2_test, y1_train, y1_test = train_test_split(
     x1, x2, y1, shuffle=True, train_size=0.7
 )
@@ -31,55 +39,57 @@ from tensorflow.keras.layers import Dense, Input
 
 #모델 1
 input1 = Input(shape=(3,))
-dense1_1 = Dense(100, activation='relu', name="dense1_1")(input1) 
+dense1_1 = Dense(10, activation='relu', name="dense1_1")(input1) 
 dense1_2 = Dense(50, activation='relu', name="dense1_2")(dense1_1) 
-dense1_3 = Dense(70, activation='relu', name="dense1_3")(dense1_2)
+dense1_3 = Dense(100, activation='relu', name="dense1_3")(dense1_2)
 dense1_4 = Dense(50, activation='relu', name="dense1_4")(dense1_3)
-output1 = Dense(3)(dense1_4) #y도 100, 3임 
+dense1_5 = Dense(10, activation='relu', name="dense1_5")(dense1_4)
+output1 = Dense(3)(dense1_5) #y도 100, 3임 
 model1 = Model(inputs=input1, outputs=output1)
 #model1.summary()
 
 
-
 #모델 2
 input2 = Input(shape=(3,))
-dense2_1 = Dense(100, activation='relu', name="dense2_1")(input2) 
+dense2_1 = Dense(10, activation='relu', name="dense2_1")(input2) 
 dense2_2 = Dense(50, activation='relu', name="dense2_2")(dense2_1) 
-dense2_3 = Dense(70, activation='relu', name="dense2_3")(dense2_2)
+dense2_3 = Dense(100, activation='relu', name="dense2_3")(dense2_2)
 dense2_4 = Dense(50, activation='relu', name="dense2_4")(dense2_3)
-output2 = Dense(3)(dense2_4) 
+dense2_5 = Dense(10, activation='relu', name="dense2_5")(dense2_4)
+output2 = Dense(3)(dense2_5)
 model2 = Model(inputs=input2, outputs=output2)
 #model2.summary()
 
 
-
-#모델 병합, concatenate
-#성능 다 똑같지만, 대문자와 소문자는 사용 방법이 틀리다
+#모델 병합 = concatenate
+#성능은 다 같지만 대문자와 소문자는 사용 방법이 다르다
 
 from tensorflow.keras.layers import Concatenate, concatenate
 # from keras.layers.merge import Concatenate, concatenate
 # from keras.layers import Concatenate, concatenate
 
-#2개 이상 합칠 땐 list 사용*****
+#요소를 2개 이상 합칠 땐 list 사용
 #밑에서 합쳐진다 
+
+
 #함수형 모델
 #middle1 = Dense(7)(middle1)
-#middle1 = Dense(11)(middle1)
-#이래도 된다! 
+#middle1 = Dense(11)(middle1) 이렇게 해도 된다
+
 
 #merge1 = concatenate([output1, output2])
 #class니까 생성자를
 #Concatenate(axis=1)
 merge1 = Concatenate()([output1, output2])
 middle1 = Dense(30)(merge1)
-middle1 = Dense(7)(middle1)
-middle1 = Dense(11)(middle1)
-
+middle1 = Dense(50)(middle1)
+middle1 = Dense(30)(middle1)
 
 
 ################ ouput 모델 구성 (분기)
-output1 = Dense(300)(middle1)
-output1 = Dense(70)(output1)
+output1 = Dense(30)(middle1)
+output1 = Dense(50)(output1)
+output1 = Dense(30)(output1)
 output1 = Dense(3, name="y1")(output1)
 
 
@@ -96,7 +106,7 @@ model.compile(loss='mse', optimizer='adam', metrics='mse')
 model.fit(
     [x1_train, x2_train],
     [y1_train],
-    epochs=200, batch_size=8,
+    epochs=100, batch_size=8,
     validation_split=0.25,
     verbose=1
 )
--- keras/test.py
+++ keras/test.py
@@ -1,36 +1,41 @@
+
 #1. 데이터
 import numpy as np
 
-#input 2개
-x1 = np.array([range(1,101), range(711, 811), range(100)])
-x2 = np.array([range(1,101), range(761, 861), range(100)])
+# input 1개
+x1=np.array([range(1,101), range(711, 811), range(100)])
 
-# output 3개 
-y1 = np.array((range(101,201), range(311,411), range(100)))
-y2 = np.array((range(501,601), range(431,531), range(100,200)))
-y3 = np.array((range(501,601), range(431,531), range(100,200)))
+# output 3개
+y1=np.array([range(101,201), range(311, 411), range(100)])
+y2=np.array([range(501,601), range(431,531), range(100,200)])
+y3=np.array([range(501,601), range(431,531), range(100,200)])
 
-x1 = x1.T
-y1 = y1.T
-x2 = x2.T
-y2 = y2.T
-y3 = y3.T
+x1=np.transpose(x1)
 
-#train_test_split 2개만 쓰기
+y1=np.transpose(y1)
+y2=np.transpose(y2)
+y3=np.transpose(y3)
+
+print(x1.shape) 
+print(y1.shape)
+print(y2.shape)
+print(y3.shape)
+
 from sklearn.model_selection import train_test_split
-x1_train, x1_test, y1_train, y1_test = train_test_split(
-    x1,y1, shuffle=True, train_size=0.7
+x1_train, x1_test = train_test_split(
+    x1, shuffle=True, train_size=0.6
 )
 
 from sklearn.model_selection import train_test_split
-x2_train, x2_test, y2_train, y2_test, y3_train, y3_test = train_test_split(
-    x2,y2,y3, shuffle=True, train_size=0.7
+y1_train, y1_test, y2_train,y2_test, y3_train, y3_test = train_test_split(
+    y1,y2,y3, shuffle=True, train_size=0.6
 )
 
 # from sklearn.model_selection import train_test_split
 # y3_train, y3_test = train_test_split(
-#     y3, shuffle=True, train_size=0.7
+#     y3, shuffle=True, train_size=0.2
 # )
+
 
 #2. 함수형 모델 2개 구성
 
@@ -42,53 +47,66 @@ input1 = Input(shape=(3,))
 dense1_1 = Dense(10, activation='relu', name='king1')(input1)
 dense1_2 = Dense(7, activation='relu', name='king2')(dense1_1)
 dense1_3 = Dense(5, activation='relu', name='king3')(dense1_2)
-output1 = Dense(3, activation='linear', name='king4')(dense1_3)
+output_1 = Dense(3, activation='linear', name='king4')(dense1_3)
+model1 = Model(inputs=input1, outputs=output_1)
+
+# model1.summary()
 
 # 모델2
-input2 = Input(shape=(3,))
-dense2_1 = Dense(15, activation='relu', name='queen1')(input2)
-dense2_2 = Dense(11, activation='relu', name='queen2')(dense2_1)
-output2 = Dense(3, activation='linear', name='queen3')(dense2_2) #activation='linear'인 상태
+# input2 = Input(shape=(3,))
+# dense2_1 = Dense(15, activation='relu', name='qeen1')(input2)
+# dense2_2 = Dense(11, activation='relu', name='qeen2')(dense2_1)
+# output2 = Dense(3, activation='linear', name='qeen3')(dense2_2) #activation='linear'인 상태
 
-#----------------------------------------------------------------------------------
+# model2 = Model(inputs=input2, outputs=output2)
+
+# model2.summary()
 
 #모델 병합, concatenate
-from tensorflow.keras.layers import Concatenate, concatenate
+# from tensorflow.keras.layers import Concatenate, concatenate
+# from keras.layers.merge import Concatenate, concatenate
+# from keras.layers import Concatenate, concatenate
 
-merge1 = Concatenate()([output1, output2])
+#대문자는 클래스 
+# merge1 = concatenate([output1, output2]) #2개 이상이라 list로 묶습니다
+# merge1 = Concatenate()([output1, output2]) 대문자로 쓰기 위한 방법 1
+# merge1 = Concatenate(axis=1)([output1, output2]) 대문자로 쓰기 위한 방법 2
 
-middle1 = Dense(30)(merge1)
-middle1 = Dense(7)(middle1)
-middle1 = Dense(11)(middle1)
+# middle1 = Dense(30)(merge1)
+# middle2 = Dense(7)(middle1)
+# middle3 = Dense(11)(middle2)
+
+#이름 이것도 가능 (다만, 가독성 위해 이름을 middle 1, 2, 3)
+# middle1 = Dense(30)(merge1)
+# middle1 = Dense(7)(middle1)
+# middle1 = Dense(11)(middle1)
 
 ################# output 모델 구성 (분기)
-output1 = Dense(30)(middle1)
-output1 = Dense(7)(output1)
-output1 = Dense(3)(output1)
+output1 = Dense(30)(output_1)
+output1_1 = Dense(7)(output1)
+output1_2 = Dense(3)(output1_1)
 
-output2 = Dense(15)(middle1)
+output2 = Dense(15)(output_1)
 output2_1 = Dense(14)(output2)
 output2_3 = Dense(11)(output2_1)
 output2_4 = Dense(3)(output2_3)
 
-output3 = Dense(30)(middle1)
-output3_1 = Dense(20)(output3)
-output3_3 = Dense(10)(output3_1)
+output3 = Dense(20)(output_1)
+output3_1 = Dense(14)(output3)
+output3_3 = Dense(11)(output3_1)
 output3_4 = Dense(3)(output3_3)
 
-# 모델 정의
-model = Model(inputs = [input1, input2], 
-              outputs = [output1, output2_4, output3_4])
+#2 모델 정의
+model = Model(inputs = input1, 
+              outputs = [output1_2, output2_4,output3_4])
 
 model.summary()
 
-# 컴파일 , 훈련
+#3.컴파일 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
-model.fit([x1_train, x2_train], 
-            [y1_train, y2_train, y3_train], 
-            epochs=100, batch_size=7, validation_split=0.25, verbose=1)
+model.fit([x1_train], [y1_train, y2_train, y3_train], epochs=100, batch_size=8,
+           validation_split=0.25, verbose=1)
 
-# 평가
-result = model.evaluate([x1_test, x2_test], [y1_test, y2_test, y3_test], batch_size=7)
+result = model.evaluate(x1_test,[y1_test,y2_test, y3_test], batch_size=8)
 
-print("result :", result)
+print("result: ", result)
