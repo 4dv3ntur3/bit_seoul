16a49aca6457202029519bd84e7d389e8a607034
allyep13.07@gmail.com <allyep13.07@gmail.com>
2020-11-18 23:35:04 +0900

    mnist -> CNN: load ModelCheckPoints

--- keras/keras52_load_checkpoint.py
+++ keras/keras52_load_checkpoint.py
@@ -1,67 +1,25 @@
 #2020-11-18 (8일차)
-#load checkpoint (*.hdf5)
+#mnist -> CNN: load checkpoint (*.hdf5)
 
 
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.datasets import mnist #텐서플로우에서 제공해 준다(수치로 변환해서 제공)
+from tensorflow.keras.datasets import mnist
 
-#train_test_split 할 필요 없이 알아서 나눠 준다
-(x_train, y_train), (x_test, y_test) = mnist.load_data() #괄호 주의
-
-#60000장 * 28pixel * 28pixel
-# print(x_train.shape, x_test.shape) #(60000, 28, 28)(10000, 28, 28)
-# print(y_train.shape, y_test.shape) #(60000, )      (10000,)        : 스칼라
+#1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data() 
 
 
-# print(x_train[0])
-# print(y_train[1]) #label 
-
-
-
-# plt.imshow(x_train[0], 'gray')
-# plt.show()
-
-
-#8은 2보다 4배의 가치? 3은 1보다 3배의 가치? no
-#One-Hot Encoder
-#y_train: 60000, -> OneHotEncoding : 1 0 0 0 0 0 0 0 0 0 (60000, 10) : 분류가 10개니까 (0~9)
-
-
-
-#1. 데이터 전처리: OneHotEncoding 대상은 Y
+#전처리: OneHotEncoding 대상은 Y
 from tensorflow.keras.utils import to_categorical
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
-print(y_train)
 
-
-# print(y_train.shape, y_test.shape)
-# print(y_train[0]) #y_train[0]=5 -> [0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]
-
-
-#shape 바꿀 줄 알아야 함
-#60000, 14, 14, 4도 가능하고 60000, 28, 14, 2도 가능
-#LSTM으로도 바꿀 수 있다
 
 x_train = x_train.reshape(60000, 28, 28, 1).astype('float32')/255.
 x_test = x_test.reshape(10000, 28, 28, 1).astype('float32')/255.
-                        #x_test.shape[0], x_test.shape[1] ... 
+                      
 
-
-
-#predict data, answer data
-x_predict = x_train[20:30]
-y_answer = y_train[20:30]
-
-
-
-#CNN에 넣을 수 있는 4차원 reshape + y도 onehotencoding
-#scaler 사용해야: 어떤 게 더 좋을지는 해 봐야 안다
-#지금 이 상황에서 M은 255라는 걸 알고 있음. 그러므로 MinMax에서는 255로 나누면 0~1 사이로 수렴 가능
-
-
-# print(x_train[0]) 
 
 
 
@@ -82,7 +40,6 @@ from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 #3. 컴파일, 훈련
 from tensorflow.keras.models import load_model
 model = load_model('./model/mnist-10-0.0694.hdf5')
-
 #checkpooint에는 모델과 가중치가 같이 저장된다
 
 
