8cac368a5383b50eb8e64813404c63de155424ee
allyep13.07@gmail.com <allyep13.07@gmail.com>
2020-11-18 23:34:26 +0900

    모델+가중치 저장 / 가중치만 저장

--- keras/keras51_1_save_weights.py
+++ keras/keras51_1_save_weights.py
@@ -1,69 +1,30 @@
 #2020-11-18 (8일차)
-#가중치 save
+#fit 이후 model.save(): 모델+가중치 저장
+#fit 이후 model_save_weights(): 가중치만 저장
 
 
 
-
-import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.datasets import mnist #텐서플로우에서 제공해 준다(수치로 변환해서 제공)
+from tensorflow.keras.datasets import mnist
 
-#train_test_split 할 필요 없이 알아서 나눠 준다
-(x_train, y_train), (x_test, y_test) = mnist.load_data() #괄호 주의
-
-#60000장 * 28pixel * 28pixel
-# print(x_train.shape, x_test.shape) #(60000, 28, 28)(10000, 28, 28)
-# print(y_train.shape, y_test.shape) #(60000, )      (10000,)        : 스칼라
+#1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 
-# print(x_train[0])
-# print(y_train[1]) #label 
-
-
-
-# plt.imshow(x_train[0], 'gray')
-# plt.show()
-
-
-#8은 2보다 4배의 가치? 3은 1보다 3배의 가치? no
-#One-Hot Encoder
-#y_train: 60000, -> OneHotEncoding : 1 0 0 0 0 0 0 0 0 0 (60000, 10) : 분류가 10개니까 (0~9)
-
-
-
-#1. 데이터 전처리: OneHotEncoding 대상은 Y
+#전처리: OneHotEncoding 대상은 Y
 from tensorflow.keras.utils import to_categorical
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
-print(y_train)
 
-
-# print(y_train.shape, y_test.shape)
-# print(y_train[0]) #y_train[0]=5 -> [0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]
-
-
-#shape 바꿀 줄 알아야 함
-#60000, 14, 14, 4도 가능하고 60000, 28, 14, 2도 가능
-#LSTM으로도 바꿀 수 있다
 
 x_train = x_train.reshape(60000, 28, 28, 1).astype('float32')/255.
 x_test = x_test.reshape(10000, 28, 28, 1).astype('float32')/255.
-                        #x_test.shape[0], x_test.shape[1] ... 
-
 
 
 #predict data, answer data
-x_predict = x_train[20:30]
-y_answer = y_train[20:30]
+# x_predict = x_train[20:30]
+# y_answer = y_train[20:30]
 
-
-
-#CNN에 넣을 수 있는 4차원 reshape + y도 onehotencoding
-#scaler 사용해야: 어떤 게 더 좋을지는 해 봐야 안다
-#지금 이 상황에서 M은 255라는 걸 알고 있음. 그러므로 MinMax에서는 255로 나누면 0~1 사이로 수렴 가능
-
-
-# print(x_train[0]) 
 
 
 #2. 모델
@@ -71,31 +32,15 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 
 model = Sequential()
-model.add(Conv2D(30, (2, 2), padding='same', input_shape=(28, 28, 1))) #padding 주의!
+model.add(Conv2D(30, (2, 2), padding='same', input_shape=(28, 28, 1)))
 model.add(Conv2D(50, (2, 2), padding='valid'))
-model.add(Conv2D(120, (3, 3))) #padding default=valid
+model.add(Conv2D(120, (3, 3)))
 model.add(Conv2D(200, (2, 2), strides=2))
 model.add(Conv2D(30, (2, 2)))
-model.add(MaxPooling2D(pool_size=2)) #pool_size default=2
+model.add(MaxPooling2D(pool_size=2)) 
 model.add(Flatten()) 
-model.add(Dense(10, activation='relu')) #flatten하면서 곱하고 dense에서 또 100 곱함 
-                                        #Conv2d의 activation default='relu'
-                                        #LSTM의 activation default='tanh'
-#MaxPooling2D-Flatten:reshape 개념
-
-model.add(Dense(10, activation='softmax')) #softmax** : 2 이상 분류(다중분류)의 activation은 softmax, 2진분류는 sigmoid
-                                            #즉 softmax를 사용하려면 OneHotEncoding 해야
-# model.summary()
-
-
-
-
-######모델 저장
-# model.save('./save/model_test01_1.h5')
-
-
-
-
+model.add(Dense(10, activation='relu')) 
+model.add(Dense(10, activation='softmax')) 
 
 
 
@@ -145,10 +90,10 @@ hist = model.fit(x_train, y_train, epochs=100, batch_size=32, verbose=1,
 
 
 #모델 + 가중치 저장
-model.save('./save/model_test02_2.h5')
+model.save('./save/model_test02_2.h5') #keras51_2_load_weights.py
 
 #가중치만 저장
-model.save_weights('./save/weight_test02.h5')
+model.save_weights('./save/weight_test02.h5') #keras51_3_load_weights.py
 
 
 
@@ -161,9 +106,6 @@ val_acc = hist.history['val_accuracy']
 
 
 #4. 평가, 예측
-
-#얘네는 evaluate에서 나온 애들
-
 result = model.evaluate(x_test, y_test, batch_size=32)
 print("loss: ", result[0])
 print("acc: ", result[1])
@@ -171,60 +113,16 @@ print("acc: ", result[1])
 
 
 
-# #시각화
-# #plot에는 x, y가 들어간다 (그래야 그래프가 그려짐)
-# import matplotlib.pyplot as plt
-
-# plt.figure(figsize=(10, 6)) 
-# #단위 뭔지 찾아볼 것!!!
-# #pyplot.figure 는 매개 변수에 주어진 속성으로 새로운 도형을 생성합니다. 
-# #figsize 는 도형 크기를 인치 단위로 정의합니다.
-
-
-# plt.subplot(2, 1, 1) #2, 1, 1 -> 두 장 중의 첫 번째의 첫 번째 (2행 1열에서 첫 번째)
-# # plt.plot(hist.history['loss'],) #loss값이 순서대로 감
-# plt.plot(hist.history['loss'], marker='.', c='red', label='loss') #loss값이 순서대로 감, 마커는 점, 색깔은 빨간색, 라벨은 loss
-# plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
-
-# plt.grid() #모눈종이 배경
-# plt.title('loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-
-# #위에서 라벨 명시 후 위치 명시
-# #그림의 위치(location)는 상단: label:loss, label:val_loss 이 둘이 박스로 해서 저 위치에 나올 것
-# plt.legend(loc='upper right')
-
-
-
-
-# plt.subplot(2, 1, 2) #2, 1, 1 -> 2행 1열 중 두 번째 (두 번째 그림)
-# # plt.plot(hist.history['loss'],) #loss값이 순서대로 감
-# plt.plot(hist.history['accuracy'], marker='.', c='red') #loss값이 순서대로 감, 마커는 점, 색깔은 빨간색, 라벨은 loss
-# plt.plot(hist.history['val_accuracy'], marker='.', c='blue')
-
-# plt.grid() #모눈종이 배경
-# plt.title('accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-
-# #여긴 라벨만 명시
-# plt.legend(['accuracy', 'val_accuracy'])
-
-# #보여 줘
-# plt.show()
-
-
 #정답
-y_answer = np.argmax(y_answer, axis=1)
+# y_answer = np.argmax(y_answer, axis=1)
 
 #예측값
-y_predict = model.predict(x_predict)
-y_predict = np.argmax(y_predict, axis=1)
+# y_predict = model.predict(x_predict)
+# y_predict = np.argmax(y_predict, axis=1)
 
 
-print("예측값: ", y_predict)
-print("정답: ", y_answer)
+# print("예측값: ", y_predict)
+# print("정답: ", y_answer)
 
 
 
