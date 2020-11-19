0734dc64d69cf1865b6b9a16ab08625275e3fe1a
allyep13.07@gmail.com <allyep13.07@gmail.com>
2020-11-19 18:26:22 +0900

    2020-11-19 과제

--- keras/homework.py
+++ keras/homework.py
@@ -6,20 +6,28 @@
 import numpy as np
 
 dataset = np.array(range(1, 10))
-# dataset = np.array([range(11), range(100, 200), range(1000, 2000)])
 
-dataset = np.array([range(101, 201), range(311, 411), range(100)])
+#개수가 똑같아야 한다 
+dataset = np.array([range(100), range(100, 200), range(500, 600)])
 
+# dataset = np.array([range(101, 201), range(311, 411), range(100)])
+
+# print(dataset)
+
+
+
+dataset = dataset.T
 size = 5
 
 
 #data가 2차원일 경우
 def split_x(dataset, size):
     aaa = [] #는 테스트
-    seq = dataset.shape[1]
+    seq = dataset.shape[0]
 
+    print(seq)
     for i in range(seq-size+1):
-        subset = dataset[:, i:(i+size)]
+        subset = dataset[i:(i+size), :]
 
         #aaa.append 줄일 수 있음
         #소스는 간결할수록 좋다
@@ -28,9 +36,8 @@ def split_x(dataset, size):
         
         
         
-    print(type(aaa))
+    # print(type(aaa))
     return np.array(aaa)
 
-dataset = split_x(dataset, size)
-print("=============")
+dataset = split_x(dataset, 5)
 print(dataset)
